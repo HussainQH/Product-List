dfb7296c19cfa59ccb661624a2f6033ecdcf2f6d
HussainQH <hussainqh@gmail.com>
2021-10-24 23:03:35 +0300

    with delete(Not working)

--- app.js
+++ app.js
@@ -3,12 +3,42 @@ const products = require("./data");
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+app.use(express.json());
 
 app.get("/api/products", (req, res) => {
   res.json(products);
+});
+
+app.get("/api/products/:productId", (req, res) => {
+  const product = products.find(
+    (product) => product.id === +req.params.productId
+  );
+  res.json(product);
+});
+
+app.post("/api/products", (req, res) => {
+  const newProduct = products.push(req.body);
+  res.status(201).json(newProduct);
+});
+
+app.delete("/api/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  const foundProduct = products.find((product) => product.id === +productId);
+
+  if (foundProduct) {
+    const tempProducts = products.filter(
+      (product) => product.id !== foundProduct.id
+    );
+
+    res.json(tempProducts);
+  } else {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`The application is running on localhost:${PORT}`);
 });
 
 module.exports = app;
